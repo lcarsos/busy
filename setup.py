1bc3121547f3dfbcfeecbd19d5f27026dcee0b30
Ezekiel Chopper <lcarsos@lcarsos.com>
2017-08-09 22:29:07 -0600

    I switched to README.md without updating this. oops

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from busy import version
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
